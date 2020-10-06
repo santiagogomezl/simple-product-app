91dac010260d7461fcd4d78ec0fc2a5b78c5a58d
Santiago Gomez <santiagogomezl@Santiagos-MacBook-Pro.local>
2020-10-06 08:38:19 -0700

    Simple Product App Client v2.1

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // API_ENDPOINT: `https://hidden-journey-35899.herokuapp.com/api/products`,
+  API_ENDPOINT: `https://hidden-journey-35899.herokuapp.com/api/products`,
   API_KEY: process.env.REACT_APP_API_KEY,
-  API_ENDPOINT: 'http://localhost:8000/api/products'
+  //API_ENDPOINT: 'http://localhost:8000/api/products'
 }
